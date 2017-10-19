2bd2376a7f40004e64250a3194dbc9097df20452
etiennemustow <etienne.o.mustow@gmail.com>
2017-10-19 17:05:30 +0100

    Articles can be created and show on a new page, test passes

--- spec/features/restaurants_spec.rb
+++ spec/features/restaurants_spec.rb
@@ -1,12 +1,22 @@
 require 'rails_helper'
 
 feature 'restaurants' do
-  context 'No restaurants' do
     scenario 'Visiting /new should return correct page' do
-    visit '/restaurants/new'
+      visit '/restaurants/new'
 
-    expect(page).to have_content 'Add a new restaurant!'
-    expect(page).to have_css('input[type="text"]')
+      expect(page).to have_content 'Add a new restaurant!'
+      expect(page).to have_css('input[type="text"]')
     end
-  end
+    scenario 'Adding a new restaurant should display on page' do
+      visit '/restaurants/new'
+      fill_in 'restaurant[name]', with: "Etis Diner"
+      fill_in 'restaurant[description]', with: "A diner"
+      click_button 'Save Restaurant'
+
+      visit '/restaurants/1'
+      expect(page).to have_content "Name: Etis Diner"
+      expect(page).to have_content "Description: A diner"
+    end
+
+
  end
